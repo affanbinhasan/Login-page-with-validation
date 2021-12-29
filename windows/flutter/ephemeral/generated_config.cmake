79fd820fb6ceb2c1a1bdb540dc81eaad0fc84770
AFAN BIN HASAN <affanumarbh@outlook.com>
2021-12-30 01:00:35 +0530

    uploaded all

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\DELL\\Desktop\\Smartserv Frontend WEB APP\\rest_api_integration" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\DELL\\Desktop\\Smartserv Frontend WEB APP\\rest_api_integration"
+  "FLUTTER_ROOT=C:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\DELL\\Desktop\\Smartserv Frontend WEB APP\\rest_api_integration\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\DELL\\Desktop\\Smartserv Frontend WEB APP\\rest_api_integration"
+  "FLUTTER_TARGET=C:\\Users\\DELL\\Desktop\\Smartserv Frontend WEB APP\\rest_api_integration\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\DELL\\Desktop\\Smartserv Frontend WEB APP\\rest_api_integration\\.dart_tool\\package_config.json"
+)
